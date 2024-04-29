06b7c3505548f78c417cf04eceae4c8a6f7f7f85
Ilya <cherry_pashtet@mail.ru>
2024-04-30 02:15:43 +0300

    fix: implemented a callback for deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,28 +7,38 @@ const placeList = document.querySelector('.places__list'); // место для 
 const addButton = mainContent.querySelector('.profile__add-button'); // кнопка добавления карточки
 const cardTemplate = document.querySelector('#card-template').content; // разметка карточки
 
+
 // @todo: Функция создания карточки
 
-function createCard(cardTitleDescription, cardImageLink) {
+// const deleteCard = delButton.addEventListener('click', () => {
+//     cardElement.remove();
+// });
+
+function createCard(cardTitleDescription, cardImageLink, {deleteCard} ) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const delButton = cardElement.querySelector('.card__delete-button');
-
     cardElement.querySelector('.card__image').src = cardImageLink;
     cardElement.querySelector('.card__title').textContent = cardTitleDescription;
     cardElement.querySelector('.card__image').alt = cardTitleDescription;
 
 // @todo: Функция удаления карточки
 
-    delButton.addEventListener('click', () => {
-        cardElement.remove();
-    });
+    delButton.addEventListener('click', deleteCard);
 
     return cardElement;
 }
 
+// @todo: Функция удаления карточки
+
+function deleteCard(evt) {
+    let currentCard = evt.target.closest('.card');
+    currentCard.remove();
+};
+
+
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((element) => {
-    const cardElement = createCard(element.name, element.link);
+    const cardElement = createCard(element.name, element.link, {deleteCard});
     placeList.append(cardElement);
 });
